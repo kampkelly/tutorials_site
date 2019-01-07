3b57b0817a3a337d80dc11ef4f6da7f8284f312e
Runor Adjekpiyede <kampkellykeys@gmail.com>
2019-01-07 12:49:09 +0100

    ft(connect-database): connects the database

---
+++ server/config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+module.exports = {
+    development: {
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DEVELOPMENT,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: "postgres",
+      operatorsAliases: false
+    },
+    test: {
+      username: "root",
+      password: null,
+      database: "database_test",
+      host: "127.0.0.1",
+      dialect: "mysql"
+    },
+    production: {
+      username: "root",
+      password: null,
+      database: "database_production",
+      host: "127.0.0.1",
+      dialect: "mysql"
+    }
+}
---
+++ server/models/tutorials.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Tutorials = sequelize.define('Tutorials', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {});
+  Tutorials.associate = function(models) {
+    // associations can be defined here
+  };
+  return Tutorials;
+};
